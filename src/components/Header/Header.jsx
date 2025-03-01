89d5756005dc5ed6c0a9e7cfdfcee637a9407798
zuri <zurimanes@gmail.com>
2025-03-01 03:26:28 +0100

    Agrego todas las ideas de paginas para el sitio web

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+    const [showMedia, setShowMedia] = useState(false);
+    const [showEvents, setShowEvents] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+
     return (
-        <>
-            <header>
-                <NavLink to={"/"}>Home</NavLink>
-                <NavLink to={"/tapeonsale"}>Tape on Sale</NavLink>
-                <NavLink to={"/musica"}>Música</NavLink>
-                <NavLink to={"/fotos"}>Fotos</NavLink>
-                <NavLink to={"/videos"}>Vídeos</NavLink>
-                <NavLink to={"/eventos"}>Próximos eventos</NavLink>
-            </header>
-        </>
+        <header>
+            <NavLink to={"/"} activeClassName="active">
+                Home
+            </NavLink>
+            <NavLink to={"/about"} activeClassName="active">
+                Tape on Sale
+            </NavLink>
+
+            <div
+                className="dropdown"
+                onMouseEnter={() => setShowMedia(true)}
+                onMouseLeave={() => setShowMedia(false)}
+            >
+                <span>Media</span>
+                {showMedia && (
+                    <div className="dropdown-content">
+                        <NavLink to={"/musica"} activeClassName="active">
+                            Música
+                        </NavLink>
+                        <NavLink to={"/videos"} activeClassName="active">
+                            Vídeos
+                        </NavLink>
+                        <NavLink to={"/fotos"} activeClassName="active">
+                            Fotos
+                        </NavLink>
+                    </div>
+                )}
+            </div>
+
+            <div
+                className="dropdown"
+                onMouseEnter={() => setShowEvents(true)}
+                onMouseLeave={() => setShowEvents(false)}
+            >
+                <span>Eventos y Merch</span>
+                {showEvents && (
+                    <div className="dropdown-content">
+                        <NavLink to={"/eventos"} activeClassName="active">
+                            Próximos eventos
+                        </NavLink>
+                        <NavLink to={"/merchandising"} activeClassName="active">
+                            Merch
+                        </NavLink>
+                    </div>
+                )}
+            </div>
+
+            <div
+                className="dropdown"
+                onMouseEnter={() => setShowMore(true)}
+                onMouseLeave={() => setShowMore(false)}
+            >
+                <span>Más</span>
+                {showMore && (
+                    <div className="dropdown-content">
+                        <NavLink to={"/contacto"} activeClassName="active">
+                            Contacto
+                        </NavLink>
+                        <NavLink to={"/social-media"} activeClassName="active">
+                            Social Media
+                        </NavLink>
+                        <NavLink to={"/FAQ"} activeClassName="active">
+                            FAQ
+                        </NavLink>
+                    </div>
+                )}
+            </div>
+        </header>
     );
 }
 
