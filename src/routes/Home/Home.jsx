de37bb7d919dd9f8f061e8a04ba11d4bd85973a4
zuri <zurimanes@gmail.com>
2025-03-06 17:12:25 +0100

    Agrego gif de fondo a Home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,13 +3,25 @@ import "./Home.css";
 
 function Home() {
     return (
-        <>
-            <div className="homeContainer">
-                <h1>Tape on Sale</h1>
+        <div className="homeContainer">
+            <div
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundImage: "url(./assets/fondoHome.gif)",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    opacity: 0.3,
+                    zIndex: -1,
+                }}
+            />
 
-                <h2>No vendemos cintas</h2>
-            </div>
-        </>
+            <h1>Tape on Sale</h1>
+            <h2>No vendemos cintas</h2>
+        </div>
     );
 }
 
