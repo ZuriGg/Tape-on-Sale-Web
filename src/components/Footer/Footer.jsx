342196be889e320f3d570a1d59db522057245b9a
zuri <zurimanes@gmail.com>
2025-03-08 17:57:49 +0100

    Actualizo enlaces e imagenes de footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Footer.css";
-import { Outlet } from "react-router-dom";
 
 function Footer() {
     return (
@@ -8,66 +7,100 @@ function Footer() {
             <footer>
                 <p className="categoria">Contacto:</p>
                 <p>tapeonsale@gmail.com</p>
-                <img
+                <a
+                    href="https://tapeonsale.bandcamp.com/"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Amazon Music.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/BandCamp.jpg" alt="" />
+                </a>
+
+                <a
+                    href="https://open.spotify.com/intl-es/artist/0PZxntvCechoXQ5OTvk4wN"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Apple Music.png"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Spotify.png" alt="" />
+                </a>
+
+                <a
+                    href="https://www.youtube.com/channel/UCCY-TQlvFITIY4JpfxWHo_Q"
                     className="iconoFooter"
-                    src="assets/img/Iconos/BandCamp.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Youtube.png" alt="" />
+                </a>
+
+                <a
+                    href="https://music.youtube.com/channel/UCbNrKkraoIKgjyhMlFSlTMw"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Deezer.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Youtube Music.jpg" alt="" />
+                </a>
+
+                <a
+                    href="https://www.instagram.com/tapeonsale/"
                     className="iconoFooter"
-                    src="assets/img/Iconos/FaceBook.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Instagram.png" alt="" />
+                </a>
+
+                <a
+                    href="https://www.tiktok.com/@tapeonsale"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Instagram.png"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/TikTok.jpg" alt="" />
+                </a>
+
+                <a
+                    href="https://music.amazon.com/artists/B0CBR32LLK/tape-on-sale"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Patreon.png"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Amazon Music.jpg" alt="" />
+                </a>
+
+                <a
+                    href="https://music.apple.com/es/artist/tape-on-sale/1697220331"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Spotify.png"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Apple Music.png" alt="" />
+                </a>
+
+                <a
+                    href="https://www.facebook.com/profile.php?id=61560278122987"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Tidal.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/FaceBook.jpg" alt="" />
+                </a>
+
+                <a
+                    href="https://www.patreon.com/user?u=129192499&fromConcierge=true"
                     className="iconoFooter"
-                    src="assets/img/Iconos/TikTok.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Patreon.png" alt="" />
+                </a>
+
+                <a
+                    href="https://listen.tidal.com/artist/40649238"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Youtube Music.jpg"
-                    alt=""
-                />
-                <img
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Tidal.jpg" alt="" />
+                </a>
+                <a
+                    href="https://www.deezer.com/es/artist/221465365"
                     className="iconoFooter"
-                    src="assets/img/Iconos/Youtube.png"
-                    alt=""
-                />
+                    target="_blank"
+                >
+                    <img src="assets/img/Iconos/Deezer.jpg" alt="" />
+                </a>
             </footer>
         </>
     );
